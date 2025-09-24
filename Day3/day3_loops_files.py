535543408bd94800fe62ae104e874aefd86c58e7
sajid <saji.iq@gmail.com>
2025-09-24 19:00:32 +0500

    Add Day3 folder with loops and practice code

---
+++ Day3/day3_loops_files.py
@@ -0,0 +1,28 @@
+
+print("For loop from 1 to 5:")
+for i in range(1, 6):
+    print(i)
+
+print("\nWhile loop example:")
+count = 5
+while count > 0:
+    print("Count", count)
+    count -= 1
+
+print("\nBreak and Continue Example:")
+for i in range(10):
+    if i == 5:
+        break
+    if i % 2 == 0:
+        continue
+    print("Odd number:", i)
+# writting
+with open("sample.txt", "w") as f:
+    f.write("Hello, this is Day 3!\n")
+    f.write("We are learning loops and file handling.\n")
+
+# reading
+with open("sample.txt", "r") as f:
+    content = f.read()
+    print("\nFile Content:")
+    print(content)
---
+++ Day3/practice_code.py
@@ -0,0 +1,23 @@
+print("Practicing loops")
+
+# for loop
+num = int(input("Enter Number: "))
+print(f"Multiplication of (num): ")
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+# while loop
+nums = int(input("Enter Number: "))
+while nums > 0:
+    print(nums)
+    nums -= 1
+    print("Time up")
+for a in range(21):
+    if a % 2 != 0:
+        print(a)
+sum = 0
+while True:
+    count = int(input("Enter Number: "))
+    if count == 0:
+        break
+    sum += count
+print("sum=", sum)
